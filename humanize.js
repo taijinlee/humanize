da5bddc335275af094b680e2598832b8460ffe13
Yan-Fa Li <yanfali@gmail.com>
2013-07-09 12:53:43 -0700

    Make relative time use new catalog

--- humanize.js
+++ humanize.js
@@ -325,50 +325,50 @@
 
     // within 2 seconds
     if (timeDiff < 2 && timeDiff > -2) {
-      return (timeDiff >= 0 ? 'just ' : '') + 'now';
+      return (timeDiff >= 0 ? humanize.catalog['just '] : '') + humanize.catalog['now'];
     }
 
     // within a minute
     if (timeDiff < 60 && timeDiff > -60) {
-      return (timeDiff >= 0 ? Math.floor(timeDiff) + ' seconds ago' : 'in ' + Math.floor(-timeDiff) + ' seconds');
+      return (timeDiff >= 0 ? Math.floor(timeDiff) + humanize.catalog[' seconds ago'] : humanize.catalog['in '] + Math.floor(-timeDiff) + humanize.catalog[' seconds');
     }
 
     // within 2 minutes
     if (timeDiff < 120 && timeDiff > -120) {
-      return (timeDiff >= 0 ? 'about a minute ago' : 'in about a minute');
+      return (timeDiff >= 0 ? humanize.catalog['about a minute ago'] : humanize.catalog['in about a minute']);
     }
 
     // within an hour
     if (timeDiff < 3600 && timeDiff > -3600) {
-      return (timeDiff >= 0 ? Math.floor(timeDiff / 60) + ' minutes ago' : 'in ' + Math.floor(-timeDiff / 60) + ' minutes');
+      return (timeDiff >= 0 ? Math.floor(timeDiff / 60) + humanize.catalog[' minutes ago'] : humanize.catalog['in '] + Math.floor(-timeDiff / 60) + humanize.catalog[' minutes']);
     }
 
     // within 2 hours
     if (timeDiff < 7200 && timeDiff > -7200) {
-      return (timeDiff >= 0 ? 'about an hour ago' : 'in about an hour');
+      return (timeDiff >= 0 ? humanize.catalog['about an hour ago'] : humanize.catalog['in about an hour']);
     }
 
     // within 24 hours
     if (timeDiff < 86400 && timeDiff > -86400) {
-      return (timeDiff >= 0 ? Math.floor(timeDiff / 3600) + ' hours ago' : 'in ' + Math.floor(-timeDiff / 3600) + ' hours');
+      return (timeDiff >= 0 ? Math.floor(timeDiff / 3600) + humanize.catalog[' hours ago'] : humanize.catalog['in '] + Math.floor(-timeDiff / 3600) + humanize.catalog[' hours']);
     }
 
     // within 2 days
     var days2 = 2 * 86400;
     if (timeDiff < days2 && timeDiff > -days2) {
-      return (timeDiff >= 0 ? '1 day ago' : 'in 1 day');
+      return (timeDiff >= 0 ? humanize.catalog['1 day ago'] : 'in 1 day');
     }
 
     // within 29 days
     var days29 = 29 * 86400;
     if (timeDiff < days29 && timeDiff > -days29) {
-      return (timeDiff >= 0 ? Math.floor(timeDiff / 86400) + ' days ago' : 'in ' + Math.floor(-timeDiff / 86400) + ' days');
+      return (timeDiff >= 0 ? Math.floor(timeDiff / 86400) + humanize.catalog[' days ago'] : humanize.catalog['in '] + Math.floor(-timeDiff / 86400) + humanize.catalog[' days']);
     }
 
     // within 60 days
     var days60 = 60 * 86400;
     if (timeDiff < days60 && timeDiff > -days60) {
-      return (timeDiff >= 0 ? 'about a month ago' : 'in about a month');
+      return (timeDiff >= 0 ? humanize.catalog['about a month ago'] : humanize.catalog['in about a month']);
     }
 
     var currTimeYears = parseInt(humanize.date('Y', currTime), 10);
@@ -379,15 +379,15 @@
     // within a year
     var monthDiff = currTimeMonths - timestampMonths;
     if (monthDiff < 12 && monthDiff > -12) {
-      return (monthDiff >= 0 ? monthDiff + ' months ago' : 'in ' + (-monthDiff) + ' months');
+      return (monthDiff >= 0 ? monthDiff + humanize.catalog[' months ago'] : humanize.catalog['in '] + (-monthDiff) + humanize.catalog[' months']);
     }
 
     var yearDiff = currTimeYears - timestampYears;
     if (yearDiff < 2 && yearDiff > -2) {
-      return (yearDiff >= 0 ? 'a year ago' : 'in a year');
+      return (yearDiff >= 0 ? humanize.catalog['a year ago' : humanize.catalog['in a year']);
     }
 
-    return (yearDiff >= 0 ? yearDiff + ' years ago' : 'in ' + (-yearDiff) + ' years');
+    return (yearDiff >= 0 ? yearDiff + humanize.catalog[' years ago'] : humanize.catalog['in '] + (-yearDiff) + humanize.catalog[' years']);
   };
 
   /**
